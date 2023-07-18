4e5ae17a259e102787b636250229a2d41cedad86
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2023-07-18 12:00:25 -0600

    polygon 3

--- main.cpp
+++ main.cpp
@@ -18,37 +18,11 @@ void render(Framebuffer &framebuffer) {
   const Color yellow = Color(255, 255, 0);
   const Color blue = Color(0, 0, 255);
 
-  std::vector<Vertex> p1 = {
-    {165, 380},
-    {185, 360},
-    {180, 330},
-    {207, 345},
-    {233, 330},
-    {230, 360},
-    {250, 380},
-    {220, 385},
-    {205, 410},
-    {193, 383}
-  };
-
-  std::vector<Vertex> p2 = {
-    {321, 335},
-    {288, 286},
-    {339, 251},
-    {374, 302}
-  };
-
   std::vector<Vertex> polygon3 = {
     {377, 249},
     {411, 197},
     {436, 249}
   };
-
-  framebuffer.createPolygon(p1, white);
-  framebuffer.fillPolygon(p1, yellow);
-
-  framebuffer.createPolygon(p2, white);
-  framebuffer.fillPolygon(p2, blue);
 
   framebuffer.createPolygon(polygon3, white);
   framebuffer.fillPolygon(polygon3, red);
